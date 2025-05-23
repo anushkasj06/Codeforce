7db53ed30b053d126ec8618fea17dd4eb5793647
Anushka <anushkasjadhav1@gmail.com>
2025-05-23 19:06:08 +0530

    today's codeforce

--- B_Li_Hua_and_Pattern.java
+++ B_Li_Hua_and_Pattern.java
@@ -1,50 +1,50 @@
 import java.util.*;
 
 public class B_Li_Hua_and_Pattern {
-    public static void solve(int n, int b, int[][] a) {
-        int sum = 0;
+    public static void solve(Scanner scanner) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int[][] x = new int[a + 1][a + 1]; // Using 1-based indexing
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][j] != a[n - i - 1][n - j - 1]) {
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                x[i][j] = scanner.nextInt();
+            }
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                if (x[i][j] != x[a - i + 1][a - j + 1]) {
                     sum++;
                 }
             }
         }
 
-        sum = sum / 2;  // Each mismatch is counted twice
+        sum /= 2;
+
         if (sum > b) {
             System.out.println("NO");
             return;
         }
 
         b -= sum;
-        if (n % 2 == 1) {
+
+        if (a % 2 == 1) {
             System.out.println("YES");
         } else if (b % 2 == 1) {
             System.out.println("NO");
         } else {
             System.out.println("YES");
         }
-        return;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();  // Read the number of test cases
-        while (t-- > 0) {
-            int n = scanner.nextInt();  // Read the size of the matrix
-            int b = scanner.nextInt();  // Read the number of allowed changes
-            int[][] a = new int[n][n];  // Initialize the matrix
+        int testCase = scanner.nextInt();
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = scanner.nextInt();  // Read the matrix elements
-                }
-            }
-
-            solve(n, b, a);  // Call the solve function
-            
+        while (testCase-- > 0) {
+            solve(scanner);
         }
     }
 }
