2e65919f5e390bcb160fd360dcb270531db24dde
Anushka <anushkasjadhav1@gmail.com>
2025-06-25 19:29:38 +0530

    codeforce

--- D_Same_Differences.java
+++ D_Same_Differences.java
@@ -2,24 +2,36 @@ import java.util.*;
 
 public class D_Same_Differences {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+       Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();  // Number of test cases
+
         while (t-- > 0) {
-            int n  = sc.nextInt();
+            int n = sc.nextInt();  // Size of the array
             int[] arr = new int[n];
+
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
+                arr[i] -= i;  // Transform: a[i] = a[i] - i
             }
-            int pair =0;
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j] - arr[i] == j - i) {
-                        pair++;
-                    }
-                }
+
+            // Map to store frequency of each transformed value
+            Map<Integer, Long> freq = new HashMap<>();
+
+            for (int val : arr) {
+                freq.put(val, freq.getOrDefault(val, 0L) + 1);
             }
-            
-            System.out.println(pair);
+
+            long ans = 0;
+
+            for (long count : freq.values()) {
+                ans += (count * (count - 1)) / 2;  // nC2
+            }
+
+            System.out.println(ans);
         }
+
+        sc.close();
+        
     }
 }
